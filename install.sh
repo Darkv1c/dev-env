36f9ec499c16a83e14c620602afb10a5234b0e3a
Víctor Alfonso Orellano Calambás <74632476+Darkv1c@users.noreply.github.com>
2025-12-23 14:31:33 -0500

    Add Neovim configuration file copy to install script
    Copy nvim.lua to the Neovim configuration directory.

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-x86_6
 chmod +x nvim-linux-x86_64.appimage
 ./nvim-linux-x86_64.appimage --appimage-extract 
 mv ./squashfs-root/usr/bin/nvim /usr/bin/nvim
+cp ./nvim.lua ~/.config/nvim/init.lua
 
 # Set up oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
